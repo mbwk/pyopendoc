7df2616746b0c47838f5f95064566da3069c80cc
mbwk <mbwkarl@gmail.com>
2016-08-10 16:47:50 +0800

    Correct setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="pyopendoc",
-    version="0.3",
+    version="0.4",
     description="Open Office Document manipulation module",
     url="http://redapesolutions.com/",
     author="Red Ape Solutions Sdn Bhd",
     author_email="karl@redapesolutions.com",
-    packages=['pyopendoc', 'test'],
+    packages=find_packages(),
     zip_safe=False)
 
