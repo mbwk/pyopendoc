a9f510fddc31f444f3423579451bad6adb134734
Jafnee <jafnee@redapesolutions.com>
2016-09-27 19:11:58 +0800

    bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
     name="pyopendoc",
-    version="0.4.1",
+    version="0.4.2",
     description="Open Office Document manipulation module",
     classifiers=[
         'Development Status :: 3 - Alpha'
     ],
     url="https://github.com/mbwk/pyopendoc",
-    download_url="https://github.com/mbwk/pyopendoc//tarball/0.4.1",
+    download_url="https://github.com/mbwk/pyopendoc/tarball/0.4.2",
     author="Red Ape Solutions Sdn Bhd",
     author_email="karl@redapesolutions.com",
     packages=find_packages(),
